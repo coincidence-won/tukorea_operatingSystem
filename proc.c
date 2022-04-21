a553d60c9a6719d3f8c38c8dd238b055c60b8ae2
dndus567 <dndus567@naver.com>
2022-04-22 01:54:03 +0900

    xv6-ps system call_reports

--- proc.c
+++ proc.c
@@ -88,7 +88,7 @@ allocproc(void)
 found:
   p->state = EMBRYO;
   p->pid = nextpid++;
-
+  p->priority = 10;
   release(&ptable.lock);
 
   // Allocate kernel stack.
@@ -322,14 +322,14 @@ wait(void)
 void
 scheduler(void)
 {
-  struct proc *p;
+  struct proc *p, *p1;
   struct cpu *c = mycpu();
   c->proc = 0;
   
   for(;;){
     // Enable interrupts on this processor.
     sti();
-
+    struct proc *highP;
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
@@ -339,6 +339,14 @@ scheduler(void)
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
+      highP = p;
+      for(p1 = ptable.proc; p1 < &ptable.proc[NPROC]; p1++){
+	      if(p1->state != RUNNABLE)
+	      continue;
+	      if(highP->priority > p1->priority)
+              highP = p1;
+      }
+      p = highP;
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
@@ -531,4 +539,23 @@ procdump(void)
     }
     cprintf("\n");
   }
+}
+
+int cps()
+{
+	struct proc *p;
+	sti();
+
+	acquire(&ptable.lock);
+	cprintf("name \t pid \t state \t priority \n");
+	for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+		if(p->state == RUNNING)
+			cprintf("%s \t %d \t SLEEPING \t %d \n",p->name,p->pid,p->priority);
+		else if(p->state == RUNNING)
+			cprintf("%s \t %d \t RUNNING \t %d \n",p->name,p->pid,p->priority);
+		else if(p->state == RUNNABLE)
+			cprintf("%s \t %d \t RUNNABLE \t %d \n",p->name,p->pid,p->priority);
+}
+release(&ptable.lock);
+return 22;
 }
